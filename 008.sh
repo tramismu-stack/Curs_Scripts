d6ca3a9d16b6ffd0181ae08cf9cf9796a403f8b8
ToniR <tramismu@esliceu.net>
2026-04-21 16:26:45 +0200

    Video 21 y 22 While y For

--- 008.sh
+++ 008.sh
@@ -6,9 +6,10 @@ echo "El PATH: $(dirname ${0} ) el fitxer seria $(basename ${0}) "
 
 NUMERO_DE_PARAMETRES=${#}
 
-if [[ ${NUMERO_DE_PARAMETRE} -eq 0 ]]
+if [[${NUMERO_DE_PARAMETRES} -eq 0 ]]
 then
     echo "Usage: ${0} USER_NAME [USER_NAME] ..."
+    exit 1
 fi
 
 echo "${*}"
@@ -21,5 +22,12 @@ do
     echo "${USER_NAME}:${PASSWORD}"
     echo
 done
-X = 1
-while [[ ]]
+
+X=1
+while [[ ${#} -ge 1 ]]
+do
+    USER_NAME=${1}
+    PASSWORD=$(date +%s%N | sha256sum | head -c10)
+    echo "${USER_NAME}:${PASSWORD}"
+    shift
+done
